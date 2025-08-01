c1a2080b32829f3b1e67433e8c58339d50b8ffcd
Paul Horwitz <paulhorwitz@Pauls-laptop.lan>
2025-08-01 17:14:23 -0400

    Fix scoring message - use 'your turn' instead of player name when opponent scores

--- script.js
+++ script.js
@@ -1192,8 +1192,7 @@ class MultiplayerManager {
             message = `You scored ${points} point${points !== 1 ? 's' : ''}. It is ${opponentName}'s turn`;
             color = this.myPlayerSymbol === 'O' ? '#3182ce' : '#e53e3e';
         } else {
-            const myName = this.myPlayerName || 'you';
-            message = `${this.opponentPlayerName || 'Your opponent'} scored ${points} point${points !== 1 ? 's' : ''}. It is ${myName}'s turn`;
+            message = `${this.opponentPlayerName || 'Your opponent'} scored ${points} point${points !== 1 ? 's' : ''}. It is your turn`;
             color = this.myPlayerSymbol === 'O' ? '#3182ce' : '#e53e3e';
         }
 
